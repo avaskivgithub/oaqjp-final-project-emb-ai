dc46152ab3d055afffe850a155065d384b5e9494
Andriana <andriana.vaskiv@gmail.com>
2025-02-22 22:44:26 -0500

    Task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +7,20 @@ def emotion_detector(text_to_analyse):
     data = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json = data, headers=headers)
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
     
-    return response.text
+    return {
+                'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+                'dominant_emotion': max(emotions, key=emotions.get)
+            }
